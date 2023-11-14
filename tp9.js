4e9f1234c0b5563d3d725897a9c38fc3686af9a3
romainGuerin0 <144117771+romainGuerin0@users.noreply.github.com>
2023-11-14 12:21:53 +0100

    Add files via upload

--- tp9.js
+++ tp9.js
@@ -56,3 +56,23 @@ switch (color)
     break;
 }
 }
+
+function seconnecter3(){
+    var i = 0;
+    do {
+        var id= prompt("Entrez votre nom d'utilisateur");
+        var mdp= prompt("Entrez votre mot de passe");
+        if (id == "admin" && mdp =="admin")
+        {
+            document.write=("Bievenue" +id);
+            break;
+        }
+        else
+        alert("Accès refusé");
+    i +=1;
+
+    } while (i<3);
+
+    if (i==3)
+    alert("Délai dépassé");
+}
